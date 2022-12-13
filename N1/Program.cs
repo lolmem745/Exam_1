1899a5c33edb5d2575518f410611c3649053f42a
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-12-13 10:34:11 +0300

    Problem solved

--- N1/Program.cs
+++ N1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void printArray(string[] arr)
+{
+    Console.Write($"[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}");
+        if (i != arr.Length - 1) Console.Write($", ");
+    }
+    Console.Write($"]");
+}
+static string[] lengthChecker(string[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length > 3) arr[i] = String.Empty;
+    }
+    string[] arr1 = arr.Where(x => x != String.Empty).ToArray();
+    return arr1;
+}
+
+string[] arr = { "2387", "23", "234", "82732", "992993", "449", "Kazan", "-2", "fly" };
+
+arr = lengthChecker(arr);
+printArray(arr);
